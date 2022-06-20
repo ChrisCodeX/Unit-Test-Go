64f79f907309edde559264f007084a4a8b7e8522
Christian <3003html@gmail.com>
2022-06-19 21:21:30 -0500

    Function to test Addition modified

--- main_test.go
+++ main_test.go
@@ -3,8 +3,19 @@ package main
 import "testing"
 
 func TestAddition(t *testing.T) {
-	total := Addition(5, 5)
-	if total != 10 {
-		t.Errorf("Addition was incorrect, got %d expected %d", total, 10)
+	tables := []struct {
+		a int
+		b int
+		n int
+	}{
+		{1, 2, 3},
+		{2, 2, 4},
+		{25, 26, 51},
+	}
+	for _, item := range tables {
+		total := Addition(item.a, item.b)
+		if total != item.n {
+			t.Errorf("Addition was incorrect, got %d expected %d", total, 10)
+		}
 	}
 }
